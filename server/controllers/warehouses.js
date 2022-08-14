523abc3f4416c3c2fd23b5f78b1d850f2b7d2bd5
thinhphuoc <63508962+thinhphuoc@users.noreply.github.com>
2022-08-14 11:13:19 +0700

    Update API info

--- server/controllers/warehouses.js
+++ server/controllers/warehouses.js
@@ -1,5 +1,6 @@
 import { WarehouseModel } from "../models/WarehouseModel.js";
 import { ProductModel } from "../models/ProductModel.js";
+
 export const createWarehouse = async (req, res) => {
   try {
     const { warehouse_id, hotline, address } = req.body;
@@ -33,6 +34,76 @@ export const listWarehouse = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+
+export const findProductID = async (req, res) => {
+  try {
+    //query in DB
+    const features = await WarehouseModel.findOne({'products.product_id': req.query})
+      // .filtering()
+      // .sorting()
+      // .paginating();
+    const products = await features.query;
+    res.json({
+      status: "success",
+      result: products.length,
+      products: products,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filtering() {
+    const queryObj = { ...this.queryString }; // queryString = req.query
+
+    const excludedFields = ["page", "sort", "limit"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      //gte greater than or equal
+      // gt grater than
+      //lt lesser than
+      //lte lesser than or equal
+      (match) => "$" + match
+    );
+
+    this.query.find(JSON.parse(queryStr));
+    console.log(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sorting() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      console.log(sortBy);
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+
+    return this;
+  }
+
+  paginating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 28;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+
+}
+
 
 export const inputProduct = async (req, res) => {
   try {
--- server/routers/warehouses.js
+++ server/routers/warehouses.js
@@ -3,12 +3,14 @@ import {
   createWarehouse,
   deleteWarehouse,
   listWarehouse,
-  inputProduct
+  inputProduct,
+  findProductID
 } from "../controllers/warehouses.js";
 
 const router = express.Router();
 router.post("/create", createWarehouse);
 router.delete("/delete", deleteWarehouse);
 router.get("/", listWarehouse);
+router.get("/find",findProductID);
 router.put("/add",inputProduct)
 export default router;
