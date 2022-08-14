a415410f33e111950bec807efe905c69025b50ca
unknown <mentri1011@gmail.com>
2022-08-14 17:08:41 +0700

    update userpage

--- ec_app/src/Components/Page/UserPage/userPage.js
+++ ec_app/src/Components/Page/UserPage/userPage.js
@@ -2,6 +2,7 @@ import account from './img/account.svg'
 import ic_sup from './img/support.svg'
 import ic_exit from './img/exit.svg'
 import ic_hiss from './img/icon_hiss.svg'
+import ic_map from './img/bxs_map.svg'
 
 
 function Userpage() {
@@ -36,6 +37,14 @@ function Userpage() {
                     </div>
                     <div className="meta_info_selection_aa">
                         <div className="meta_info_selection_aa_img">
+                            <img src={ic_map} alt=""/>
+                        </div>
+                        <div className="meta_info_selection_aa_name">
+                            <p>Địa chỉ</p>
+                        </div>
+                    </div>
+                    <div className="meta_info_selection_aa">
+                        <div className="meta_info_selection_aa_img">
                             <img src={ic_sup} alt=""/>
                         </div>
                         <div className="meta_info_selection_aa_name">
@@ -52,6 +61,7 @@ function Userpage() {
                     </div>
                 </div>
             </div>
+            
             <div className="userpage_info">
                 {/* case 1: user info */}
                 <div className="userpage_user">
@@ -81,7 +91,81 @@ function Userpage() {
                             </div>
                         </div>
                     </div>
+                    <div className='userpage_user_born'>
+                        <div className='userpage_user_born_11'>
+                            <span>Ngày sinh</span>
+                        </div>
+            
+                        <div className='userpage_user_born_12'>
+                            <input className="userpage_user_born_12_date" type="date"/>
+                        </div>
+                    </div>
+                    <div className='userpage_user_born'>
+                        <div className='userpage_user_born_11'>
+                            <span>Giới tính</span>
+                        </div>
+            
+                        <div className='userpage_user_born_12'>
+                            <input type="radio" name="gender" value="male" checked /> <span>Nam</span>
+                            <input type="radio" name="gender" value="male" checked /> <span>Nữ</span>
+                            <input type="radio" name="gender" value="male" checked /> <span>Khác</span>
+                        </div>
+                    </div>
+        
+                    <div className='userpage_user_born'>
+                        <div className='userpage_user_born_11'>
+                            <span>Số điện thoại</span>
+                        </div>
+            
+                        <div className='userpage_user_born_12'>
+                            <input type="tel" name="usrtel" className="userpage_user_born_12_tel"/>
+                        </div>
+                    </div>
+                    <div className='userpage_user_born'>
+                        <div className='userpage_user_born_11'>
+                            <span>Email</span>
+                        </div>
+            
+                        <div className='userpage_user_born_12'>
+                            <input type="email" name="usrtel" className="userpage_user_born_12_tel"/>
+                        </div>
+                    </div>
+
+                    <div className='userpage_user_button'>
+                        <button>Cập nhật</button>
+                    </div>
                 </div>
+                <div className='userpage_userPass'>
+                    <h3>Thay đổi mật khẩu</h3>
+                    <div className="userpage_userPass_oo">
+                        <div className='userpage_userPass_oo_1'>
+                            <h4>Mật khẩu cũ</h4>
+                        </div>
+                        <div className='userpage_userPass_oo_2'>
+                            <input type="password" />
+                        </div>
+                    </div>
+                    <div className="userpage_userPass_oo">
+                        <div className='userpage_userPass_oo_1'>
+                            <h4>Mật khẩu mới</h4>
+                        </div>
+                        <div className='userpage_userPass_oo_2'>
+                            <input type="password" />
+                        </div>
+                    </div>
+                    <div className="userpage_userPass_oo">
+                        <div className='userpage_userPass_oo_1'>
+                            <h4>Nhập lại mật khẩu</h4>
+                        </div>
+                        <div className='userpage_userPass_oo_2'>
+                            <input type="password" />
+                        </div>
+                    </div>
+                    <div className='userpage_user_button1'>
+                        <button>Cập nhật</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
